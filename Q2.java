ce204601d9c4689c5fee7d2d64660b5cd9b6b547
kundu <kunduachyut19@gmail.com>
2023-10-10 00:03:14 +0530

    first commit

---
+++ Q2.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Q2 {
+    System.out.print("Enter:");
+    Scanner in = new Scanner(System.in);
+    String s = in.nextLine();  
+    System.out.print("The String is : "+s);
+     
+}
